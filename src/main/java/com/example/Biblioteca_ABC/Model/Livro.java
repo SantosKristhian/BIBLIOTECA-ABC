4cdf889515e6647dd12e7e1904d16431429eefec
rodrigues217 <joaoprodrigues237@gmail.com>
2025-08-03 23:17:35 -0300

    Update Livro.java

--- src/main/java/com/example/Biblioteca_ABC/Model/Livro.java
+++ src/main/java/com/example/Biblioteca_ABC/Model/Livro.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Data // Adicionando a anotação @Data para gerar automaticamente os métodos getters e setters
 public class Livro {
     private Integer id;
     private String ISSN;
@@ -14,4 +14,52 @@ public class Livro {
     private String sinopse;
     private int ano;
     private int numeroPaginas;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getISSN() {
+        return ISSN;
+    }
+
+    public void setISSN(String ISSN) {
+        this.ISSN = ISSN;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
 }
